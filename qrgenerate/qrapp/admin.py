d2f3adb4a0c2331dc773a6646718c56bdcd8883c
Brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2023-11-04 13:35:19 +0530

    Update admin.py

--- qrgenerate/qrapp/admin.py
+++ qrgenerate/qrapp/admin.py
@@ -3,6 +3,7 @@ from qrapp.models import qr
 # Register your models here.
 
 
-@admin.register(qr)
 class hello(admin.ModelAdmin):
     list_display = ('name', 'text')
+    
+admin.site.register(qr, hello)
